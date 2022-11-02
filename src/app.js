3160aa52c19c97debba0b93c16a44904a64e8837
maximiliano <maxipompas@hotmail.com>
2022-11-02 11:44:58 -0300

    agregando html

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, ()=> {console.log(`Escuchando en el puerot ${PORT}`)})
 server.on('error', error => console.log(`Error en servidor ${PORT}`))
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.send('Bienvenido a mi proyecto en la nube!')
